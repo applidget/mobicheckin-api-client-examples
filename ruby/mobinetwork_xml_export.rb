6b580b8e0c8355eb98f55d78d02d4501b653886b
Ahrry <gopalahrry@gmail.com>
2013-01-29 10:30:35 +0100

    no comment, just for push

--- ruby/mobinetwork_xml_export.rb
+++ ruby/mobinetwork_xml_export.rb
@@ -51,25 +51,25 @@ def get_xml_exhibitor_connections(exhibitor_id)
   api_url_connection("/api/v1//events/#{event_id}/exhibitors/#{exhibitor_id}/connections.xml?&auth_token=#{api_token}")
 end
 
-def candidates
+def connections
   exhibitor_ids = []
   get_xml_exhibitors.elements.each('exhibitors/exhibitor/_id') do |ele|
     exhibitor_ids << ele.text
   end
-  
+  exhibitor_ids
   exhibitor_ids.each do |exhibitor_id|
     puts get_xml_exhibitor_connections(exhibitor_id)
   end
 end
 
 def product_xml
-  
   xml = Builder::XmlMarkup.new( :indent => 2 )
   xml.instruct! :xml, :encoding => "UTF-8"
   
+  exhibitor_ids = get_exhibitor_ids
   xml.CareerFare do |carreer_fare|
-    get_xml_exhibitors.elements.each('exhibitors/exhibitor/name') do |exhibitor_name|
-      carreer_fare.HostSite exhibitor_name.text
+    get_xml_exhibitors.elements.each('exhibitors/exhibitor/_id') do |exhibitor_id|
+      carreer_fare.HostSite exhibitor_id.text
     end
   end
   
