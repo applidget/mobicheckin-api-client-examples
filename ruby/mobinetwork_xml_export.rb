6d82e6f2b7f604541596fd1564b46e846bd5e929
Ahrry <gopalahrry@gmail.com>
2013-02-04 12:06:52 +0100

    product a xml with all exhibtors and their candidates. About candidate just show email for the moment

--- ruby/mobinetwork_xml_export.rb
+++ ruby/mobinetwork_xml_export.rb
@@ -52,13 +52,41 @@ def get_xml_exhibitor_connections(exhibitor_id)
   api_url_connection("/api/v1/events/#{EVENT_ID}/exhibitors/#{exhibitor_id}/connections.xml?&auth_token=#{API_TOKEN}")
 end
 
+def get_xml_guests(page_number)
+  api_url_connection("/api/v1/events/#{EVENT_ID}/guests.xml?page=#{page_number}&auth_token=#{API_TOKEN}")
+end
+
+def guest_with_uid(uid)
+  page_number = 1
+  while get_xml_guests(page_number.to_s)
+    REXML::XPath.each(get_xml_guests(page_number.to_s), '//guest').each do |guest|
+      if guest.elements["uid"].text == uid
+        return guest
+      end
+    end
+    page_number += 1
+  end
+end
+
 def product_xml
   xml = Builder::XmlMarkup.new( :indent => 2 )
   xml.instruct! :xml, :encoding => "UTF-8"
   
   xml.CareerFare do |carreer_fare|
-    get_xml_exhibitors.elements.each('exhibitors/exhibitor/_id') do |exhibitor_id|
-      carreer_fare.HostSite exhibitor_id.text
+    REXML::XPath.each(get_xml_exhibitors, '//exhibitor').each do |exhibitor_elements|
+      xml.Exhibitor do |exhibitor|
+        exhibitor.name exhibitor_elements.elements["name"].text
+        REXML::XPath.each(get_xml_exhibitor_connections(exhibitor_elements.elements["_id"].text), '//connection').each do |connection|
+          if connection
+            xml.Candidate do |candidate|
+              if guest_with_uid(connection.elements["guest-uid"].text)
+                guest = guest_with_uid(connection.elements["guest-uid"].text)
+                candidate.email guest.elements["email"].text
+              end
+            end
+          end
+        end
+      end
     end
   end
 end
