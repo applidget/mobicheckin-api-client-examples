52c49c6b79823d3ed00e63c8cbd247ba9822ca6a
rpechayr <rpechayr@gmail.com>
2013-03-11 13:25:42 +0100

    Outputing to only one file. Mistery tag inserted at the end

--- ruby/mobinetwork_xml_export.rb
+++ ruby/mobinetwork_xml_export.rb
@@ -15,27 +15,27 @@ require "fileutils"
 
 # We need a valid API token
 API_TOKEN = ENV['MOBICHECKIN_API_TOKEN']
-unless API_TOKEN
-  puts "Could not find MOBICHECKIN_API_TOKEN in your environment"
-  abort
-end
-
+HOST_SITE = ENV['HOST_SITE']
+CC_EMAIL = ENV['CC_EMAIL']
 # We need an event id
 EVENT_ID = ENV['MOBICHECKIN_EVENT_ID']
-unless EVENT_ID
-  puts "Could not find MOBICHECKIN_EVENT_ID in your environment"
-  abort
+
+["API_TOKEN", "EVENT_ID", "CC_EMAIL", "HOST_SITE"].each do |var_name|
+  unless Kernel.const_get var_name
+    puts "Could not find #{var_name} in your environment"
+  end
 end
+  
 
 EXHIBITORS_CONNECTIONS_FOLDER = File.join(File.join(File.expand_path(File.dirname(__FILE__)), "exhibitors_connections"), EVENT_ID)
 
 def api_url_connection(url)
   request_uri = url
   
-  http = Net::HTTP.new("app.mobicheckin.com", 443)
-  http.use_ssl = true
+  http_connection = Net::HTTP.new("app.mobicheckin.com", 443)
+  http_connection.use_ssl = true
   response = nil
-  http.start do |http|
+  http_connection.start do |http|
     req = Net::HTTP::Get.new request_uri
     response = http.request req
     if response.code != "200"
@@ -74,35 +74,27 @@ end
 def get_exhibitors
   exhibitors = {}
   REXML::XPath.each(get_xml_exhibitors, '//exhibitor').each do |exhibitor|
-    exhibitors[exhibitor.elements["_id"].text] = exhibitor.elements["name"].text
+    exhibitors[exhibitor.elements["_id"].text] = { :name => exhibitor.elements["name"].text, :meta_data => exhibitor.elements["meta-data"].text  }
   end
   exhibitors
 end
 
-def get_candidate_comments(comments_xml)
-  comments = ""
+def get_candidate_comments(xml_node, comments_xml)
   REXML::XPath.each(comments_xml, 'comment').each do |comment|
-    if comments == ""
-      comments = comment.elements["content"].text
-    else
-      comments += ";" + comment.elements["content"].text
-    end
+    xml_node.RecruiterComment comment.elements["content"].text unless comment == ""
   end
-  comments
 end
 
-def build_exhibitor_xml(exhibitor_id)
-  xml = Builder::XmlMarkup.new( :indent => 2 )
-  xml.CareerFare do |career_fare|
-    career_fare.HostSite
-    REXML::XPath.each(get_xml_exhibitor_connections(exhibitor_id), '//connection').each do |connection|
-      xml.Candidate do |candidate|
-        if guest_with_uid(connection.elements["guest-uid"].text)
-          guest = guest_with_uid(connection.elements["guest-uid"].text)
-          candidate.Email guest.elements["email"].text
-          candidate.RecruiterEmail
-          candidate.CCEmail
-          candidate.RecruiterComments get_candidate_comments(connection.elements["comments"])
+def exhibitor_xml(xml_node, exhibitor_id, recruiter_email)
+  REXML::XPath.each(get_xml_exhibitor_connections(exhibitor_id), '//connection').each do |connection|
+    xml_node.Candidate do |candidate|
+      if guest_with_uid(connection.elements["guest-uid"].text)
+        guest = guest_with_uid(connection.elements["guest-uid"].text)
+        candidate.Email guest.elements["email"].text
+        candidate.RecruiterEmail recruiter_email
+        candidate.CCEmail CC_EMAIL
+        candidate.RecruiterComments do |comments|
+          get_candidate_comments(comments, connection.elements["comments"])
         end
       end
     end
@@ -115,10 +107,20 @@ def main
     FileUtils.mkdir_p EXHIBITORS_CONNECTIONS_FOLDER
   end
   
-  get_exhibitors.each_key do |exhibitor_id|
-    File.open(File.join(EXHIBITORS_CONNECTIONS_FOLDER, File.join("#{get_exhibitors[exhibitor_id].downcase.sub(' ', '_')}.xml")), 'wb') do |f|
-      f.write build_exhibitor_xml(exhibitor_id)
+  file_name = File.join(EXHIBITORS_CONNECTIONS_FOLDER, "export.xml")
+  File.open(file_name, 'wb') do |f|
+    xml = Builder::XmlMarkup.new( :indent => 2 )
+    xml.instruct! :xml, :encoding => "UTF-8"
+    xml.CareerFare do |career_fare|
+      career_fare.HostSite HOST_SITE
+      get_exhibitors.each do |exhibitor_id, payload|
+        puts payload
+        recruiter_email = payload[:meta_data] #In this usecase we have put a recruiter email
+        exhibitor_xml(career_fare, exhibitor_id, recruiter_email)
+      end
     end
+    f.write xml
+    f.close
   end
 end
 
